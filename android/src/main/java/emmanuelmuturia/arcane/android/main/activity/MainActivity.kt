24be49bef09315b3a367a322639c4447c256ea0d
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-05-21 20:40:36 +0300

    Format the code...

--- android/src/main/java/emmanuelmuturia/arcane/android/main/activity/MainActivity.kt
+++ android/src/main/java/emmanuelmuturia/arcane/android/main/activity/MainActivity.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2025 Arcane
+ *
+ * Licenced under the Apache License, Version 2.0 (the "Licence");
+ * you may not use this file except in compliance with the Licence.
+ * You may obtain a copy of the Licence at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
 package emmanuelmuturia.arcane.android.main.activity
 
 import android.os.Bundle
@@ -16,9 +31,8 @@ class MainActivity : ComponentActivity() {
             ArcaneTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-
                 }
             }
         }
--- android/src/main/java/emmanuelmuturia/arcane/android/theme/Theme.kt
+++ android/src/main/java/emmanuelmuturia/arcane/android/theme/Theme.kt
@@ -1,114 +1,125 @@
+/*
+ * Copyright 2025 Arcane
+ *
+ * Licenced under the Apache License, Version 2.0 (the "Licence");
+ * you may not use this file except in compliance with the Licence.
+ * You may obtain a copy of the Licence at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
 package emmanuelmuturia.arcane.android.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Shapes
-import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun ArcaneTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
-    val colors = if (darkTheme) {
-        darkColorScheme(
-            primary = primaryDark,
-            onPrimary = onPrimaryDark,
-            primaryContainer = primaryContainerDark,
-            onPrimaryContainer = onPrimaryContainerDark,
-            secondary = secondaryDark,
-            onSecondary = onSecondaryDark,
-            secondaryContainer = secondaryContainerDark,
-            onSecondaryContainer = onSecondaryContainerDark,
-            tertiary = tertiaryDark,
-            onTertiary = onTertiaryDark,
-            tertiaryContainer = tertiaryContainerDark,
-            onTertiaryContainer = onTertiaryContainerDark,
-            error = errorDark,
-            onError = onErrorDark,
-            errorContainer = errorContainerDark,
-            onErrorContainer = onErrorContainerDark,
-            background = backgroundDark,
-            onBackground = onBackgroundDark,
-            surface = surfaceDark,
-            onSurface = onSurfaceDark,
-            surfaceVariant = surfaceVariantDark,
-            onSurfaceVariant = onSurfaceVariantDark,
-            outline = outlineDark,
-            outlineVariant = outlineVariantDark,
-            scrim = scrimDark,
-            inverseSurface = inverseSurfaceDark,
-            inverseOnSurface = inverseOnSurfaceDark,
-            inversePrimary = inversePrimaryDark,
-            surfaceDim = surfaceDimDark,
-            surfaceBright = surfaceBrightDark,
-            surfaceContainerLowest = surfaceContainerLowestDark,
-            surfaceContainerLow = surfaceContainerLowDark,
-            surfaceContainer = surfaceContainerDark,
-            surfaceContainerHigh = surfaceContainerHighDark,
-            surfaceContainerHighest = surfaceContainerHighestDark,
-        )
-    } else {
-        lightColorScheme(
-            primary = primaryLight,
-            onPrimary = onPrimaryLight,
-            primaryContainer = primaryContainerLight,
-            onPrimaryContainer = onPrimaryContainerLight,
-            secondary = secondaryLight,
-            onSecondary = onSecondaryLight,
-            secondaryContainer = secondaryContainerLight,
-            onSecondaryContainer = onSecondaryContainerLight,
-            tertiary = tertiaryLight,
-            onTertiary = onTertiaryLight,
-            tertiaryContainer = tertiaryContainerLight,
-            onTertiaryContainer = onTertiaryContainerLight,
-            error = errorLight,
-            onError = onErrorLight,
-            errorContainer = errorContainerLight,
-            onErrorContainer = onErrorContainerLight,
-            background = backgroundLight,
-            onBackground = onBackgroundLight,
-            surface = surfaceLight,
-            onSurface = onSurfaceLight,
-            surfaceVariant = surfaceVariantLight,
-            onSurfaceVariant = onSurfaceVariantLight,
-            outline = outlineLight,
-            outlineVariant = outlineVariantLight,
-            scrim = scrimLight,
-            inverseSurface = inverseSurfaceLight,
-            inverseOnSurface = inverseOnSurfaceLight,
-            inversePrimary = inversePrimaryLight,
-            surfaceDim = surfaceDimLight,
-            surfaceBright = surfaceBrightLight,
-            surfaceContainerLowest = surfaceContainerLowestLight,
-            surfaceContainerLow = surfaceContainerLowLight,
-            surfaceContainer = surfaceContainerLight,
-            surfaceContainerHigh = surfaceContainerHighLight,
-            surfaceContainerHighest = surfaceContainerHighestLight,
-        )
-    }
+    val colors =
+        if (darkTheme) {
+            darkColorScheme(
+                primary = primaryDark,
+                onPrimary = onPrimaryDark,
+                primaryContainer = primaryContainerDark,
+                onPrimaryContainer = onPrimaryContainerDark,
+                secondary = secondaryDark,
+                onSecondary = onSecondaryDark,
+                secondaryContainer = secondaryContainerDark,
+                onSecondaryContainer = onSecondaryContainerDark,
+                tertiary = tertiaryDark,
+                onTertiary = onTertiaryDark,
+                tertiaryContainer = tertiaryContainerDark,
+                onTertiaryContainer = onTertiaryContainerDark,
+                error = errorDark,
+                onError = onErrorDark,
+                errorContainer = errorContainerDark,
+                onErrorContainer = onErrorContainerDark,
+                background = backgroundDark,
+                onBackground = onBackgroundDark,
+                surface = surfaceDark,
+                onSurface = onSurfaceDark,
+                surfaceVariant = surfaceVariantDark,
+                onSurfaceVariant = onSurfaceVariantDark,
+                outline = outlineDark,
+                outlineVariant = outlineVariantDark,
+                scrim = scrimDark,
+                inverseSurface = inverseSurfaceDark,
+                inverseOnSurface = inverseOnSurfaceDark,
+                inversePrimary = inversePrimaryDark,
+                surfaceDim = surfaceDimDark,
+                surfaceBright = surfaceBrightDark,
+                surfaceContainerLowest = surfaceContainerLowestDark,
+                surfaceContainerLow = surfaceContainerLowDark,
+                surfaceContainer = surfaceContainerDark,
+                surfaceContainerHigh = surfaceContainerHighDark,
+                surfaceContainerHighest = surfaceContainerHighestDark,
+            )
+        } else {
+            lightColorScheme(
+                primary = primaryLight,
+                onPrimary = onPrimaryLight,
+                primaryContainer = primaryContainerLight,
+                onPrimaryContainer = onPrimaryContainerLight,
+                secondary = secondaryLight,
+                onSecondary = onSecondaryLight,
+                secondaryContainer = secondaryContainerLight,
+                onSecondaryContainer = onSecondaryContainerLight,
+                tertiary = tertiaryLight,
+                onTertiary = onTertiaryLight,
+                tertiaryContainer = tertiaryContainerLight,
+                onTertiaryContainer = onTertiaryContainerLight,
+                error = errorLight,
+                onError = onErrorLight,
+                errorContainer = errorContainerLight,
+                onErrorContainer = onErrorContainerLight,
+                background = backgroundLight,
+                onBackground = onBackgroundLight,
+                surface = surfaceLight,
+                onSurface = onSurfaceLight,
+                surfaceVariant = surfaceVariantLight,
+                onSurfaceVariant = onSurfaceVariantLight,
+                outline = outlineLight,
+                outlineVariant = outlineVariantLight,
+                scrim = scrimLight,
+                inverseSurface = inverseSurfaceLight,
+                inverseOnSurface = inverseOnSurfaceLight,
+                inversePrimary = inversePrimaryLight,
+                surfaceDim = surfaceDimLight,
+                surfaceBright = surfaceBrightLight,
+                surfaceContainerLowest = surfaceContainerLowestLight,
+                surfaceContainerLow = surfaceContainerLowLight,
+                surfaceContainer = surfaceContainerLight,
+                surfaceContainerHigh = surfaceContainerHighLight,
+                surfaceContainerHighest = surfaceContainerHighestLight,
+            )
+        }
     val typography = ArcaneTypography
 
-    val shapes = Shapes(
-        small = RoundedCornerShape(7.dp),
-        medium = RoundedCornerShape(7.dp),
-        large = RoundedCornerShape(0.dp)
-    )
+    val shapes =
+        Shapes(
+            small = RoundedCornerShape(7.dp),
+            medium = RoundedCornerShape(7.dp),
+            large = RoundedCornerShape(0.dp),
+        )
 
     MaterialTheme(
         colorScheme = colors,
         typography = typography,
         shapes = shapes,
-        content = content
+        content = content,
     )
 }
