4c68f0bad4132406e3b936c963b2c725890d4bd1
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-05-21 17:43:12 +0300

    Set up the Theme...

--- android/src/main/java/emmanuelmuturia/arcane/android/theme/Theme.kt
+++ android/src/main/java/emmanuelmuturia/arcane/android/theme/Theme.kt
@@ -22,27 +22,86 @@ fun ArcaneTheme(
 ) {
     val colors = if (darkTheme) {
         darkColorScheme(
-            primary = Color(0xFFBB86FC),
-            secondary = Color(0xFF03DAC5),
-            tertiary = Color(0xFF3700B3)
+            primary = primaryDark,
+            onPrimary = onPrimaryDark,
+            primaryContainer = primaryContainerDark,
+            onPrimaryContainer = onPrimaryContainerDark,
+            secondary = secondaryDark,
+            onSecondary = onSecondaryDark,
+            secondaryContainer = secondaryContainerDark,
+            onSecondaryContainer = onSecondaryContainerDark,
+            tertiary = tertiaryDark,
+            onTertiary = onTertiaryDark,
+            tertiaryContainer = tertiaryContainerDark,
+            onTertiaryContainer = onTertiaryContainerDark,
+            error = errorDark,
+            onError = onErrorDark,
+            errorContainer = errorContainerDark,
+            onErrorContainer = onErrorContainerDark,
+            background = backgroundDark,
+            onBackground = onBackgroundDark,
+            surface = surfaceDark,
+            onSurface = onSurfaceDark,
+            surfaceVariant = surfaceVariantDark,
+            onSurfaceVariant = onSurfaceVariantDark,
+            outline = outlineDark,
+            outlineVariant = outlineVariantDark,
+            scrim = scrimDark,
+            inverseSurface = inverseSurfaceDark,
+            inverseOnSurface = inverseOnSurfaceDark,
+            inversePrimary = inversePrimaryDark,
+            surfaceDim = surfaceDimDark,
+            surfaceBright = surfaceBrightDark,
+            surfaceContainerLowest = surfaceContainerLowestDark,
+            surfaceContainerLow = surfaceContainerLowDark,
+            surfaceContainer = surfaceContainerDark,
+            surfaceContainerHigh = surfaceContainerHighDark,
+            surfaceContainerHighest = surfaceContainerHighestDark,
         )
     } else {
         lightColorScheme(
-            primary = Color(0xFF6200EE),
-            secondary = Color(0xFF03DAC5),
-            tertiary = Color(0xFF3700B3)
+            primary = primaryLight,
+            onPrimary = onPrimaryLight,
+            primaryContainer = primaryContainerLight,
+            onPrimaryContainer = onPrimaryContainerLight,
+            secondary = secondaryLight,
+            onSecondary = onSecondaryLight,
+            secondaryContainer = secondaryContainerLight,
+            onSecondaryContainer = onSecondaryContainerLight,
+            tertiary = tertiaryLight,
+            onTertiary = onTertiaryLight,
+            tertiaryContainer = tertiaryContainerLight,
+            onTertiaryContainer = onTertiaryContainerLight,
+            error = errorLight,
+            onError = onErrorLight,
+            errorContainer = errorContainerLight,
+            onErrorContainer = onErrorContainerLight,
+            background = backgroundLight,
+            onBackground = onBackgroundLight,
+            surface = surfaceLight,
+            onSurface = onSurfaceLight,
+            surfaceVariant = surfaceVariantLight,
+            onSurfaceVariant = onSurfaceVariantLight,
+            outline = outlineLight,
+            outlineVariant = outlineVariantLight,
+            scrim = scrimLight,
+            inverseSurface = inverseSurfaceLight,
+            inverseOnSurface = inverseOnSurfaceLight,
+            inversePrimary = inversePrimaryLight,
+            surfaceDim = surfaceDimLight,
+            surfaceBright = surfaceBrightLight,
+            surfaceContainerLowest = surfaceContainerLowestLight,
+            surfaceContainerLow = surfaceContainerLowLight,
+            surfaceContainer = surfaceContainerLight,
+            surfaceContainerHigh = surfaceContainerHighLight,
+            surfaceContainerHighest = surfaceContainerHighestLight,
         )
     }
-    val typography = Typography(
-        bodyMedium = TextStyle(
-            fontFamily = FontFamily.Default,
-            fontWeight = FontWeight.Normal,
-            fontSize = 16.sp
-        )
-    )
+    val typography = ArcaneTypography
+
     val shapes = Shapes(
-        small = RoundedCornerShape(4.dp),
-        medium = RoundedCornerShape(4.dp),
+        small = RoundedCornerShape(7.dp),
+        medium = RoundedCornerShape(7.dp),
         large = RoundedCornerShape(0.dp)
     )
 
